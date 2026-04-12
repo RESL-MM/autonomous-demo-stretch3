04034ae85baea8322f49c5e5e2c6b748c60eb622
sspandit14 <sspandit@usc.edu>
2026-04-11 18:51:00 -0700

    add skeleton code for wafer behaviour and remaining demo

---
+++ stretch_visual_servoing/deposit_withdraw_wafer.py
@@ -0,0 +1,65 @@
+import stretch_body.robot as rb
+import math
+import argparse
+
+def move(robot, distance: float):
+    robot.base.translate_by(distance, v_m=0.25)
+    robot.push_command()
+    print(f"waited for move by {distance}m command? {robot.wait_command(timeout=60.0)}")
+
+def rotate(robot, angle: float):
+    robot.base.rotate_by(angle)
+    robot.push_command()
+    print(f"waited for move by {angle}m command? {robot.wait_command()}")
+
+def main(dw):
+    robot = rb.Robot()
+    robot.startup()
+
+    STOW_ANGLE = (3 * math.pi)/4
+    PUPD_DIST = 0.03
+
+    # deposit/withdraw values
+    DW_HEIGHT = 1.0
+    DW_LENGTH = 0.5
+
+    if not dw:
+        # pick up/put down values
+        DW_HEIGHT = 0.8
+        DW_LENGTH = 0.3
+
+    """
+    withdraw = deposit with opposite gripper action
+    assume start with arm extending out towards machine
+
+    CHECK GRIPPER ORIENTATION-- WHEN TO STOW/UNSTOW?
+    1) raise arm to correct height
+    2) extend arm
+    2.5) can 1 and 2 be done independently? does 2 need to go up and down to avoid the machine?
+    3) lower lift to contact wafer spot
+    3.5) if DEPOSIT, then RELEASE WAFER (close gripper), else do nothing
+    4) retract arm
+    4.5) open gripper (if not open)
+    5) fully or partially stow gripper
+    """
+    robot.lift.move_to(DW_HEIGHT)
+    
+    robot.arm.move_to(DW_LENGTH)
+
+    # robot.end_of_arm.move_to(-50) # [-100, 100] => [fully closed, fully open]
+    
+    robot.lift.move_by(-PUPD_DIST)
+
+    robot.lift.move_by(PUPD_DIST)
+    robot.arm.move_to(0.0)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='Stretch3 Wafer Manipulation Tests')
+    parser.add_argument('-dw', '--DW', type=str, default='dw', help='set to dw or leave empty for deposit/withdraw behaviour, else change to anything for pick-up/put-down behaviour')
+
+    args = parser.parse_args()
+    dw = True
+    if args.DW != 'dw':
+        dw = False
+
+    main(dw)
--- stretch_visual_servoing/station_to_station.py
+++ stretch_visual_servoing/station_to_station.py
@@ -1,15 +1,22 @@
 import stretch_body.robot as rb
 import math
 
+FROM_MACHINE_TO_WTABLE = -2.5
+QUARTER_COUNTERCLOCK = math.pi/2
+QUARTER_CLOCK = -QUARTER_COUNTERCLOCK
+NEXT_TO_MACHINE_OR_TRAY = 0.9
+FROM_WTABLE_TO_TRAY = 1.4
+FROM_TRAY_TO_MACHINE = 1.1
+
 def move(robot, distance: float):
-    robot.base.translate_by(distance)
+    robot.base.translate_by(distance, v_m=0.25)
     robot.push_command()
-    robot.wait_command()
+    print(f"waited for move by {distance}m command? {robot.wait_command(timeout=60.0)}")
 
 def rotate(robot, angle: float):
     robot.base.rotate_by(angle)
     robot.push_command()
-    robot.wait_command()
+    print(f"waited for move by {angle}m command? {robot.wait_command()}")
 
 def main():
     robot = rb.Robot()
@@ -18,42 +25,46 @@ def main():
     # open machine
 
     # move from machine op to wafer station
-    move(robot, -2.5)
-
+    robot.stow()
+    move(robot, FROM_MACHINE_TO_WTABLE)
+    rotate(robot, QUARTER_CLOCK)
+    move(robot, NEXT_TO_MACHINE_OR_TRAY)
+    rotate(robot, QUARTER_COUNTERCLOCK)
     # pick up wafer
 
     # move from wafer station to machine load
-    move(robot, 1.5)
-    rotate(robot, -math.pi/2)
-    move(robot, -1)
-    rotate(robot, math.pi)
-
+    rotate(robot, QUARTER_CLOCK)
+    move(robot, -NEXT_TO_MACHINE_OR_TRAY)
+    rotate(robot, QUARTER_COUNTERCLOCK)
+    move(robot, FROM_WTABLE_TO_TRAY)
+    rotate(robot, QUARTER_CLOCK)
+    move(robot, NEXT_TO_MACHINE_OR_TRAY)
+    rotate(robot, QUARTER_COUNTERCLOCK)
+    rotate(robot, QUARTER_COUNTERCLOCK)
     # deposit wafer
 
     # move from machine load to machine op
-    move(robot, 1)
-    rotate(robot, -math.pi/2)
-    move(robot, 1)
+    rotate(robot, QUARTER_CLOCK)
+    rotate(robot, QUARTER_CLOCK)
+    move(robot, -NEXT_TO_MACHINE_OR_TRAY)
+    rotate(robot, QUARTER_COUNTERCLOCK)
+    move(robot, FROM_TRAY_TO_MACHINE)
 
-    # ideally robot will run the recipe
+    ### ideally robot will run the recipe ###
+    
     # robot opens machine
 
     # move from machine op to machine unload
-    move(robot, -1)
-    rotate(robot, math.pi/2)
-    move(robot, -1)
 
     # withdraw the wafer
 
     # move from machine unload to wafer station
-    rotate(robot, math.pi)
-    move(robot, 1)
-    rotate(robot, math.pi/2)
-    move(robot, -1.5)
 
     # place the wafer
 
     # move from wafer station to machine op
-    move(robot, 2.5)
 
-    # loop done:
+    ### loop done ###
+
+if __name__ == '__main__':
+    main()
